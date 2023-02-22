1c6f3898a0bbe211afab82665a30c03710c00837
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-22 16:23:34 +0100

    chore: update readme & config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextchakra-starter",
+  title: "peeps-generator",
   titleTemplate: "%s | nextchakra-starter",
   defaultTitle: "nextchakra-starter",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextchakra-starter.sznm.dev",
+  description: "An avatar generator using openpeeps.com images",
+  canonical: "https://peeps-generator.vercel.app",
   openGraph: {
-    url: "https://nextchakra-starter.sznm.dev",
-    title: "nextchakra-starter",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: "https://peeps-generator.vercel.app",
+    title: "peeps-generator",
+    description: "An avatar generator using openpeeps.com images",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextchakra-starter**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextchakra-starter.sznm.dev og-image",
+        url: "https://og-image.sznm.dev/**peeps-generator**.vercel.app.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
+        alt: "peeps-generator og-image",
       },
     ],
-    site_name: "nextchakra-starter",
+    site_name: "peeps-generator",
   },
   twitter: {
     handle: "@sozonome",
